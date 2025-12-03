cd24550aa07c9a34a5f918bcc27056e09096aa0b
Vebster <vebster88@gmail.com>
2025-12-03 12:45:55 +0300

    Очередные правки PDFService (2)

--- src/services/PDFService.ts
+++ src/services/PDFService.ts
@@ -807,7 +807,7 @@ export class PDFService {
     content.push({
       table: {
         headerRows: 1,
-        widths: [80, 60, 50, 65, 63, 61, 55, 71, 'auto', '*'],
+        widths: [80, 60, 50, 65, 63, 61, 55, 72, 'auto', '*'],
         body: tableBody,
       },
       layout: {
